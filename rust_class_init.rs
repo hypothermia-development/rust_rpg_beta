5e685fac959fff7c894d81d7e31fe5004c569a3a
JoeyMCPC <142174364+JoeyMCPC@users.noreply.github.com>
2024-06-27 23:57:13 -0400

    Add files via upload

---
+++ rust_class_init.rs
@@ -0,0 +1,21 @@
+fn class_init()
+{
+	let fighter = class 
+	{
+		health: 30
+		damage: 15
+		ac: 0
+	}
+	let wizard = class
+	{
+		health:15
+		damage: 30
+		ac: 0
+	}
+	let ranger = class
+	{
+		health: 15
+		damage 15:
+		ac: 30
+	}
+}
